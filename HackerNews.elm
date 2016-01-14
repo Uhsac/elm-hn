9521e70ae79b11c2084cde94f59ba0dd3cb6b08e
Uhsac <aigreaultc@gmail.com>
2016-01-14 22:06:26 +0100

    Add start-app and elm-effects, prepare tasks

--- HackerNews.elm
+++ HackerNews.elm
@@ -1,16 +1,42 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Jd exposing ((:=))
+import Task exposing (Task)
+import StartApp
+import Effects exposing (Effects)
+
+-- Constant
+
+hackerNewsTopStoriesEndpoint : String
+hackerNewsTopStoriesEndpoint = "https://hacker-news.firebaseio.com/v0/topstories.json"
+
+hackerNewsItemEndpoint : String
+hackerNewsItemEndpoint = "https://hacker-news.firebaseio.com/v0/item/"
+
+-- Type
+
+type alias Story =
+  { title : String
+  , url : String
+  }
 
 -- Model
 
-type alias Model = List String
+type alias Model = List Story
 
-model : Model
-model =
-  [ "Link 1"
-  , "Link 2"
-  , "Link 3"
+initialModel : Model
+initialModel =
+  [ {title = "Link 1", url = "http://google.com"}
+  , {title = "Link 2", url = "http://google.com"}
+  , {title = "Link 3", url = "http://google.com"}
   ]
+
+init : Model -> (Model, Effects Action)
+init model =
+  ( model
+  , Effects.none
+  )
 
 -- View
 
@@ -18,11 +44,11 @@ header : Html
 header =
   h1 [ headerStyle ] [ text "Hacker News" ]
 
-item : String -> Html
-item title =
+item : Story -> Html
+item story =
   li [ itemStyle ]
-    [ span [ itemTitleStyle ] [ text title ]
-    , span [ itemAddressStyle ] [ text "Address" ]
+    [ span [ itemTitleStyle ] [ text story.title ]
+    , span [ itemAddressStyle ] [ text story.url ]
     ]
 
 itemList : Model -> Html
@@ -32,8 +58,8 @@ itemList model =
   in
     ul [ itemListStyle ] items
 
-view : Model -> Html
-view model =
+view : Signal.Address Action -> Model -> Html
+view address model =
   div [ viewStyle ]
     [ header
     , itemList model
@@ -86,6 +112,49 @@ itemAddressStyle =
     , ("font-size", ".8em")
     ]
 
-main : Html
-main =
-  view model
+-- API
+{-
+fetchTopStories : Task Task.succeed (List String)
+fetchTopStories =
+  Http.get (Jd.list Jd.string) hackerNewsTopStoriesEndpoint
+    |> Task.onError (\_ -> Task.succeed [])
+
+fetchStory : String -> Task Task.succeed Story
+fetchStory storyId =
+  Http.get storyDecoder hackerNewsItemEndpoint ++ storyId ++ ".json"
+    |> Task.onError (\_ -> Task.succeed {})
+-}
+-- Decoder
+
+storyDecoder : Jd.Decoder Story
+storyDecoder =
+  Jd.object2 Story
+    ("title" := Jd.string)
+    ("url" := Jd.string)
+
+-- Action
+
+type Action
+  = NoOp
+
+-- Update
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    NoOp ->
+      (model, Effects.none)
+
+-- App
+
+app = StartApp.start
+  { init = init initialModel
+  , update = update
+  , view = view
+  , inputs = []
+  }
+
+main = app.html
+
+port tasks : Signal (Task Effects.Never ())
+port tasks = app.tasks
